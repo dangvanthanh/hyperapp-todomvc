72aac35e025ef3c1cd600867f56150b133578a9f
Dang Van Thanh <dangvanthanh@dangthanh.org>
2018-03-07 23:23:21 +0700

    Add pretier_standard

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,82 +1,82 @@
-import {uuid, assignTodoById} from '../utils';
-import store from '../store';
+import { uuid, assignTodoById } from '../utils'
+import store from '../store'
 
 export default {
-  input: ({value}) => state => ({
-    input: value,
+  input: ({ value }) => state => ({
+    input: value
   }),
   add: () => state => {
     const todos = state.todos.concat({
       id: uuid(),
       done: false,
       editing: false,
-      value: state.input,
-    });
-    store.save(todos);
+      value: state.input
+    })
+    store.save(todos)
     return {
       input: '',
-      todos: todos,
-    };
+      todos: todos
+    }
   },
-  editUpdate: ({uuid, value}) => (state, actions) => {
+  editUpdate: ({ uuid, value }) => (state, actions) => {
     const todos = assignTodoById(state.todos, uuid, {
       editing: false,
-      value: value,
-    });
-    store.save(todos);
+      value: value
+    })
+    store.save(todos)
     return {
-      todos: todos,
-    };
+      todos: todos
+    }
   },
-  editEnter: ({uuid, e}) => (state, actions) => {
-    actions.editEnterDbClick(uuid);
-    actions.editEnterFocus(e);
+  editEnter: ({ uuid, e }) => (state, actions) => {
+    actions.editEnterDbClick(uuid)
+    actions.editEnterFocus(e)
   },
   editEnterDbClick: uuid => (state, actions) => {
-    const todos = assignTodoById(state.todos, uuid, {editing: true});
-    store.save(todos);
+    const todos = assignTodoById(state.todos, uuid, { editing: true })
+    store.save(todos)
     return {
-      todos: todos,
-    };
+      todos: todos
+    }
   },
   editEnterFocus: e => (state, actions) => {
-    const input = e.target.parentNode.parentNode.querySelector('.edit');
-    input.focus();
+    const input = e.target.parentNode.parentNode.querySelector('.edit')
+    input.focus()
   },
-  remove: ({uuid}) => (state, actions) => {
-    const todos = state.todos.filter(t => uuid !== t.id);
-    store.save(todos);
+  remove: ({ uuid }) => (state, actions) => {
+    const todos = state.todos.filter(t => uuid !== t.id)
+    store.save(todos)
     return {
-      todos: todos,
-    };
+      todos: todos
+    }
   },
-  toggle: ({uuid}) => (state, actions) => {
+  toggle: ({ uuid }) => (state, actions) => {
     const todos = state.todos.map(
-      t => (uuid === t.id ? Object.assign({}, t, {done: !t.done}) : t),
-    );
-    store.save(todos);
+      t => (uuid === t.id ? Object.assign({}, t, { done: !t.done }) : t)
+    )
+    store.save(todos)
     return {
-      todos: todos,
-    };
+      todos: todos
+    }
   },
   toggleAll: e => (state, actions) => {
-    let isCheckedAll = e.target.previousSibling.checked;
-    isCheckedAll = !isCheckedAll;
+    let isCheckedAll = e.target.previousSibling.checked
+    isCheckedAll = !isCheckedAll
     const todos = state.todos.map(t => {
-      t.done = isCheckedAll;
-      return t;
-    });
-    store.save(todos);
+      t.done = isCheckedAll
+      return t
+    })
+    store.save(todos)
     return {
-      todos: todos,
-    };
+      todos: todos
+    }
   },
-  filter: ({value}) => (state, actions) => ({filter: value}),
+  filter: ({ value }) => (state, actions) => ({ filter: value }),
   clearCompleted: () => state => {
-    const todos = state.todos.filter(t => !t.done);
-    store.save(todos);
+    const todos = state.todos.filter(t => !t.done)
+    store.save(todos)
     return {
-      todos: todos,
-    };
-  },
-};
+      todos: todos
+    }
+  }
+}
--- src/views/todo-item.js
+++ src/views/todo-item.js
@@ -1,44 +1,46 @@
-import {h} from 'hyperapp';
-import cc from 'classcat';
+import { h } from 'hyperapp'
+import cc from 'classcat'
 
 export default props => (
   <li
     class={cc([
       'todo',
-      {completed: props.todo.done, editing: props.todo.editing},
+      { completed: props.todo.done, editing: props.todo.editing }
     ])}
-    key={props.todo.id}>
-    <div className="view">
+    key={props.todo.id}
+  >
+    <div className='view'>
       <input
-        type="checkbox"
-        class="toggle"
-        checked={props.todo.done ? true : false}
-        onclick={e => props.actions.toggle({uuid: props.todo.id})}
+        type='checkbox'
+        class='toggle'
+        checked={!!props.todo.done}
+        onclick={e => props.actions.toggle({ uuid: props.todo.id })}
       />
       <label
-        ondblclick={e => props.actions.editEnter({uuid: props.todo.id, e: e})}>
+        ondblclick={e => props.actions.editEnter({ uuid: props.todo.id, e: e })}
+      >
         {props.todo.value}
       </label>
       <button
-        class="destroy"
-        onclick={e => props.actions.remove({uuid: props.todo.id})}
+        class='destroy'
+        onclick={e => props.actions.remove({ uuid: props.todo.id })}
       />
     </div>
     <input
-      type="text"
-      class="edit"
+      type='text'
+      class='edit'
       onkeyup={e =>
         e.keyCode === 13
           ? props.actions.editUpdate({
-              uuid: props.todo.id,
-              value: e.target.value,
-            })
+            uuid: props.todo.id,
+            value: e.target.value
+          })
           : null
       }
       onblur={e =>
-        props.actions.editUpdate({uuid: props.todo.id, value: e.target.value})
+        props.actions.editUpdate({ uuid: props.todo.id, value: e.target.value })
       }
       value={props.todo.value}
     />
   </li>
-);
+)
